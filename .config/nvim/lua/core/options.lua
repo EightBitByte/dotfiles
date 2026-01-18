c71b1c8924ae3a17c1ff593611a681f4d6f71fe6
Jacob Moy <apixelatedpug@gmail.com>
2026-01-18 15:24:26 -0800

    chore: add nvim

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,9 @@
+vim.o.number = true
+vim.o.autoindent = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.hlsearch = true
+vim.o.clipboard = "unnamedplus"
+vim.o.showmode = false
+vim.cmd("colorscheme habamax")
