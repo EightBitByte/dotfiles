f453413860d914b1f0ea75887a2a456b3aa5e333
Jacob Moy <apixelatedpug@gmail.com>
2026-01-21 15:30:37 -0800

    chore: kitty + install mod

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SYMLINKS=(
-  "$HOME/.bashrc"
-  "$HOME/.config/hypr"
-  "$HOME/.config/nvim"
-  "$HOME/.config/waybar"
+  ".bashrc"
+  ".config/hypr"
+  ".config/nvim"
+  ".config/waybar"
+  ".config/kitty"
   "/etc/default/grub"
 )
 
@@ -14,7 +15,11 @@ for symlink in "${SYMLINKS[@]}"; do
   # Ensure parent directory exists (e.g. for .config/foo)
   mkdir -p "$(dirname "$target_file")"
 
-  echo "Symlinking $source_file -> $target_file"
-  # -s: symbolic, -f: force (overwrite existing)
-  ln -sf "$source_file" "$target_file"
+  if [[ $symlink == "/"* ]]; then
+    echo "Symlinking $source_file -> $target_file"
+    ln -sf "$source_file" "$target_file"
+  else
+    echo "Symlinking $source_file -> $HOME/$target_file"
+    ln -sf "$source_file" "$HOME/$target_file"
+  fi
 done
